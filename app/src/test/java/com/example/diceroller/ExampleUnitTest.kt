05ceb33782d7b89490d5b2038d0022df8d4ffefe
Pjcel11 <pierrejean.martin.pjm@gmail.com>
2023-03-20 17:27:58 -0400

    Implémentation d'un Test

--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -14,4 +14,11 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun generates_numbers(){
+        val dice = Dice(6)
+        val rollResult = dice.roll()
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
+    }
 }
