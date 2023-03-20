126447499b7d89acbbb2af6922948a67480f0fa1
Pjcel11 <pierrejean.martin.pjm@gmail.com>
2023-03-20 13:56:59 -0400

    utilisation d'une image maj en fonction tirage dé

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,19 +14,50 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
+            //Verifier le textView
             //val resultTextView: TextView = findViewById(R.id.textView)
             //resultTextView.text = "6"
+            //Verifier que le bouton fonctionne
             //val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             //toast.show()
         }
+        // Do a dice roll when the app starts
+        rollDice()
 
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
         val diceRoll = dice.roll()
+
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
+
+
+        /* //texte qui faisait ref au textView qui affichait le score puisquon va utiliser les images
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+         */
     }
 
 }
